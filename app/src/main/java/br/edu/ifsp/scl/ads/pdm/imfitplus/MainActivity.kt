3bff67b42e85a1272ba88e1093c190f44e859ac7
GattiGabriel <ggattidev@gmail.com>
2025-10-31 08:27:54 -0300

    Feat: implemented MainActivity kotlin class

--- app/src/main/java/br/edu/ifsp/scl/ads/pdm/imfitplus/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/ads/pdm/imfitplus/MainActivity.kt
@@ -1,20 +1,23 @@
 package br.edu.ifsp.scl.ads.pdm.imfitplus
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import br.edu.ifsp.scl.ads.pdm.imfitplus.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val amb: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        setContentView(amb.root)
+
+        amb.startBtn.setOnClickListener {
+            val intent = Intent(this, PersonalData::class.java)
+            startActivity(intent)
         }
     }
 }
